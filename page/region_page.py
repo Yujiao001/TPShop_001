aa7144588f1b5ffbbc75e508c6fb01c20595be83
Yujiao001 <yu_jiao@outlook.com>
2018-11-18 14:31:21 +0800

    地区页面的数量灵活化

--- page/region_page.py
+++ page/region_page.py
@@ -12,13 +12,18 @@ class RegionPage(BaseAction):
 
     commit_button = By.XPATH,"//*[@text='确定']"
 
+    region_msg=By.XPATH,"//*[@class='android.widget.TextView' and starts-with(@resource-id,'com.tpshop.malls:id/rb')]"
+
+    @allure.step("选择地区页面-点击-省、市、区/县、镇/街道")
     def click_city(self):
-        for _ in range(4):
+        region_counts = len(self.find_elements(self.region_msg))
+        for _ in range(region_counts):
             cities = self.find_elements(self.city_feature)
             random_city_index = random.randint(0,len(cities) - 1)
             cities[random_city_index].click()
             time.sleep(1)
 
+    @allure.step("选择地区页面-点击-确定按钮")
     def click_commit(self):
         self.click(self.commit_button)
 
