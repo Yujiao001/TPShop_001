a39fcba1a7b97d6c55b56be50ac2248469e7094c
Yujiao001 <yu_jiao@outlook.com>
2018-11-18 10:39:01 +0800

    设置画面追加

---
+++ page/setting_page.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+from base.base_action import BaseAction
+
+
+class SettingPage(BaseAction):
+
+    title_feature = By.ID,"com.tpshop.malls:id/titlebar_title_txtv"
+
